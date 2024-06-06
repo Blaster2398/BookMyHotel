ac1e976cfbb65bc188673fb2d4c69bcaa5f9ac94
Aryan Patel <aryanindian366@gmail.com>
2024-06-06 18:19:57 +0530

    FULL app uploaded

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+
+const Navbar = () => {
+  const { user, dispatch } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+    console.log("User logged out");
+  };
+
+  const handleClick = () => {
+    if (user) {
+    } else {
+      navigate("/login");
+    }
+  };
+
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">Booking.com</span>
+        </Link>
+        {user ? (
+          <div className="userActions">
+            <span>{user.username}</span>
+            <button onClick={handleLogout} className="navButton">Logout</button>
+          </div>
+        ) : (
+          <div className="navButtons">
+            <button className="navButton">Register</button>
+            <button onClick={handleClick} className="navButton">Login</button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
